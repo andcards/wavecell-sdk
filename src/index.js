5e13259438a2f9ce9d499d5425c663f9bed6b059
asaprokhorov <oleksandrprohr@gmail.com>
2019-05-24 19:59:12 -0400

    add error types to public api

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+import * as ERROR_TYPES from "./constants/error-types";
 import * as VERIFICATION_STATUS from "./constants/verification-status";
 
 export { default as otpCodeSend } from "./otp-code-send";
 export { default as otpCodeVerify } from "./otp-code-verify";
-export { VERIFICATION_STATUS };
+export { ERROR_TYPES, VERIFICATION_STATUS };
